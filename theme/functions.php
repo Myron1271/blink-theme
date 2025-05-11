f9644185aa4e4449046143aae6075a8e413e8d74
Myron <myronmax_seelen@live.nl>
2025-05-11 21:09:16 +0200

    Update for child themes

--- theme/functions.php
+++ theme/functions.php
@@ -211,6 +211,12 @@ add_theme_support('align-wide');
 
 //    include(get_stylesheet_directory().'/includes/cpt/cpt-example.php');
 
+// FIX FOR CHILD THEME
+include(get_template_directory() . '/includes/widget-areas.php');
+include(get_template_directory() . '/includes/utilities.php');
+include(get_template_directory() . '/parts/blocks/blocks.php');
+
+// OLD CODE
 include(get_stylesheet_directory() . '/includes/widget-areas.php');
 include(get_stylesheet_directory() . '/includes/utilities.php');
 
